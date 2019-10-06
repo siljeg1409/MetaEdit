4727c55ff35f17926d18d0d467309bf0502b584e
AnyX <edis.siljegovic@gmail.com>
2019-10-06 10:01:49 +0200

    just a simple win app for editing .mp3 metadata

---
+++ MetaEdit/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace MetaEdit
+{
+    partial class frmMetaEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMetaEdit));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnFD = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Folder";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtFile
+            // 
+            this.txtFile.Enabled = false;
+            this.txtFile.Location = new System.Drawing.Point(64, 23);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(339, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // btnFD
+            // 
+            this.btnFD.Location = new System.Drawing.Point(409, 23);
+            this.btnFD.Name = "btnFD";
+            this.btnFD.Size = new System.Drawing.Size(47, 20);
+            this.btnFD.TabIndex = 2;
+            this.btnFD.Text = "...";
+            this.btnFD.UseVisualStyleBackColor = true;
+            this.btnFD.Click += new System.EventHandler(this.btnFD_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(64, 113);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(339, 20);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(64, 139);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(339, 20);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Title:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.Location = new System.Drawing.Point(64, 165);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.Size = new System.Drawing.Size(339, 20);
+            this.txtArtist.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Artist:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFile
+            // 
+            this.lblFile.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblFile.Location = new System.Drawing.Point(-2, 78);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(509, 23);
+            this.lblFile.TabIndex = 9;
+            this.lblFile.Text = "......mp3";
+            this.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(64, 191);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(165, 35);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(234, 191);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(165, 35);
+            this.btnNext.TabIndex = 11;
+            this.btnNext.Text = "Next ->";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(-2, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(506, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Drag and drop .mp3 file here";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frmMetaEdit
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 249);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.txtArtist);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFD);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMetaEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MetaEdit";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMetaEdit_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMetaEdit_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnFD;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtArtist;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MetaEdit/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetaEdit
+{
+    public partial class frmMetaEdit : Form
+    {
+        private string[] _mp3Files;
+        private int  _counter = 0;
+        private TagLib.File _meta;
+        private string _folder;
+
+        public frmMetaEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFD_Click(object sender, EventArgs e)
+        {
+            _counter = 0;
+            _folder = "";
+            _meta = null;
+            _mp3Files = null;
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtFile.Text = fbd.SelectedPath;
+                    _mp3Files = Directory.GetFiles(fbd.SelectedPath,"*.mp3");
+                    loadFile(0);
+                }
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            loadFile(++_counter);
+        }
+
+        private void loadFile(int c)
+        {
+            if (!checkFile(c)) return;
+           
+
+            lblFile.Text = Path.GetFileName(_mp3Files[c]);
+
+            _meta = TagLib.File.Create(_mp3Files[c]);
+            txtArtist.Text = (_meta.Tag.Performers.Count() > 0) ? _meta.Tag.Performers[0].ToString() : "";
+            txtTitle.Text = _meta.Tag.Title;
+            txtFileName.Text = lblFile.Text.Replace(".mp3","");
+
+        }
+
+        private bool checkFile(int c)
+        {
+
+            if (_mp3Files.Count() > c)
+            {
+                btnSave.Enabled = true;
+                _folder = Path.GetDirectoryName(_mp3Files[c]);
+            }
+            else
+            {
+                btnSave.Enabled = false;
+                btnNext.Enabled = false;
+                lblFile.Text = "Please select the folder.";
+                txtArtist.Text = "";
+                txtFileName.Text = "";
+                txtTitle.Text = "";
+                return false;
+            }
+
+            if (_mp3Files.Count() > 0) btnNext.Enabled = true;
+            if (Path.GetExtension(_mp3Files[c]) != ".mp3")
+            {
+                _mp3Files[c] = "";
+                checkFile(++c);
+            }
+
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            _meta.Tag.Performers = new String[1] { txtArtist.Text };
+            _meta.Tag.Title = txtTitle.Text;
+            _meta.Save();
+            string newFile = _folder + "\\" + txtFileName.Text + ".mp3";
+            if (!File.Exists(_meta.Name))
+            {
+                MessageBox.Show("There is no file: \n" + _meta.Name);
+                return;
+            }
+
+            //if (File.Exists(newFile)) File.Delete(newFile);
+            File.Move(_meta.Name, newFile);
+            loadFile(++_counter);
+        }
+
+        private void frmMetaEdit_DragDrop(object sender, DragEventArgs e)
+        {
+            _mp3Files = (string[])e.Data.GetData(DataFormats.FileDrop,false);
+           
+            loadFile(0);
+        }
+
+        private void frmMetaEdit_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
